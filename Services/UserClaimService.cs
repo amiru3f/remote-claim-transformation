d4e3b6158dbbaf2e0197e29a4df639baff3b2de8
Amir Solhi <>
2023-07-09 22:59:18 +0200

    zero allocation logging added

--- Services/UserClaimService.cs
+++ Services/UserClaimService.cs
@@ -3,8 +3,14 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace RemoteClaimTransformer.Services;
 
+internal static partial class LoggerExtensions
+{
+    [LoggerMessage(0, LogLevel.Information, "going to fetch claims using {sub} or {token}", EventName = nameof(GoingToFetchClaimsForToken))]
+    public static partial void GoingToFetchClaimsForToken(this ILogger logger, string sub, string token);
+}
 internal sealed class UserClaimService
 {
+
     private readonly ILogger<UserClaimService> _logger;
     public UserClaimService(IMemoryCache memoryCache, ILogger<UserClaimService> logger)
     {
@@ -17,8 +23,15 @@ internal sealed class UserClaimService
     public ValueTask<IEnumerable<Claim>> GetPermissionsByJwtTokenOrSubAsync(string sub, string jwtToken)
     {
         _ = sub;
-        _logger.LogTrace("going to fetch user claims for token: {token}", jwtToken);
+
+        _logger.GoingToFetchClaimsForToken(sub, jwtToken);
         const string permClaimName = Auth.Constants.PermissionClaimName;
-        return ValueTask.FromResult<IEnumerable<Claim>>(new List<Claim>() { new(permClaimName, "P1"), new(permClaimName, "P2") });
+
+        return ValueTask.FromResult<IEnumerable<Claim>>(new List<Claim>()
+        {
+            new(permClaimName, "P1"),
+            new(permClaimName, "P2")
+        });
     }
+
 }
