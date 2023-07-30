c0be67551116bda5cbef82fd9bdb091473c026ba
amiru3f <solhi.amir1371@gmail.com>
2023-07-30 17:04:40 +0200

    CompositFormat usage

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using RemoteClaimTransformer.Auth;
+using System.Text;
+using System.Globalization;
 
 namespace RemoteClaimTransformer.Controllers;
 
 public class TestController : Controller
 {
+    private static readonly CompositeFormat _format;
+
+    static TestController()
+    {
+        _format = CompositeFormat.Parse("Access granted to {0}");
+    }
+
     [Route("access-through-controller")]
     [RequireClaim(Constants.PermissionClaimName, Constants.P1_Permission)]
-    public string Index() => $"Access granted to {User!.Identity!.Name}";
+    public string Index() => string.Format<string>(CultureInfo.InvariantCulture, _format, User!.Identity!.Name!);
+    //This generic is not required here -----^----- put it there for the sake of readability. Hit F12 to see the implementation
 }
